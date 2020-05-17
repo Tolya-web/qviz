9cdf41652eda2cc618cbfa704daeb69bd6fe3080
Tolya <Alinka87@inbox.ru>
2020-05-17 23:51:47 +0300

    qq

--- script.js
+++ script.js
@@ -32,7 +32,6 @@ function deleteItem(){ //Удаление вопроса после ответа
 }
 
 document.getElementById('kviz').addEventListener('click', createItems);
-document.getElementById('translate').addEventListener('click', trans);
 console.log(newArr);
 
 if(SpeechRecognition){
@@ -117,6 +116,7 @@ if(SpeechRecognition){
                 console.log('Количество ответов да = ', yesCounter);
                 console.log('Количество ответов нет = ', noCounter);
                 microBtn.click();
+                
 
             }
         
@@ -139,58 +139,3 @@ if(SpeechRecognition){
     console.log('no')
 }
 
-
-function trans()
-{
-    yesStr = "yes";
-    noStr = "no";
-    let tr = yandexTranslate(document.querySelector('#start-text').innerText);
-    document.querySelector('#finish-text').innerText = tr;
-    console.log(tr);
-/*
-const MY_KEY = 'trnsl.1.1.20200517T131111Z.6ed31806d5c80498.c467337b7c047446738cc7f82af0f70705c412d3';
-
-//let words = document.querySelector('#start-text').innerText;
-let words = 'Привет';
-
-fetch('https://translate.yandex.net/api/v1.5/tr.json/translate?key='+KEY_YANDEX_TRANSLATE+'&text='+str_new+'&lang=ru-en')
-.then(
-    response => {
-         console.log(response.json()); 
-         response.json().then(
-            data =>{
-                alert(data.text[0]);
-                //console.log(data.text[0]);
-                //document.querySelector('#end-text').innerText = data.text[0];
-            }
-        )
-    }
-)
-.catch(
-    () =>{
-        console.log('Status Error!!!');
-    }*/
-
-};
-
-function yandexTranslate(translateText){
-    let url = "https://translate.yandex.net/api/v1.5/tr.json/translate";
-    //let translateText = document.querySelector('#start-text').innerText;
-    let res = ' ';
- fetch(url, {
-        method: 'POST',
-        body:'key=trnsl.1.1.20200517T131111Z.6ed31806d5c80498.c467337b7c047446738cc7f82af0f70705c412d3&lang=ru-en&text='+ translateText,
-        headers: new Headers({
-            'Content-Type': 'application/x-www-form-urlencoded'
-        })
-
-    }).then( 
-        resp=>resp.json().then(data=>
-            {
-                document.querySelector('#finish-text').innerText = data.text[0];
-                console.log(data.text[0])
-                //res =  data.text[0];
-            })
-    );
-    //return res;
-}
